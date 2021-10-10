ba1fed2467e918ccf0066b47b6003b47e7edab34
vivit <kenji@vivitjp.com>
2021-10-10 19:20:00 +0900

    first commit

---
+++ src/Exercise.tsx
@@ -0,0 +1,20 @@
+const Exercise = () => {
+
+  const var_a: string = "Example"
+
+
+
+
+
+
+
+
+
+  const result: any = var_a;
+
+  return (
+    <div>{result}</div>
+  )
+}
+
+export default Exercise
